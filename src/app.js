d9284da9f82e1767ce2bc7df1e055cc6bc13fad8
VictorSamines <saminesvictor@gmail.com>
2022-11-06 20:38:56 -0600

    Instalacion de mongoose y ejs

--- src/app.js
+++ src/app.js
@@ -5,12 +5,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 //middleware
-app.use(express.json()); //req. body
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Pagina de Inicio");
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
