9218023a667f406fa18cfae94962804ce626c158
Jeferson <jvelasquezc15@miumg.edu.gt>
2022-11-08 20:47:11 -0600

    Primera prueba con EJS

--- src/app.js
+++ src/app.js
@@ -3,13 +3,23 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+// template engine
+app.set('view engine', 'ejs')
+// specify the views directory
+app.set('views', __dirname + '/views')
 
-//middleware
-app.use(express.json());
+app.use(express.static('public'))
+//middleware -> express.json()
+// Sirve para trabajar con solicitudes put o post
+// porque en ambas solicitudes está enviando datos (en forma de algún objeto de datos)
+// al servidor y le está pidiendo al servidor que acepte o almacene esos datos (objeto),
+// que se incluye en el cuerpo (es decir req.body) de esa solicitud (POST o PUT)
+// app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Pagina de Inicio");
+  res.render("index", { titulo: 'EJS jefferson'})
 });
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
