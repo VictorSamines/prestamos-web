6304f1755afadf2625139a15353fdc15c40625d4
MicaelaAQ <mariamicaelaajcabulquievac@gmail.com>
2022-11-10 02:40:07 -0600

    Formulario y tablas terminadas

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
 
+// Motor de plantilla
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(__dirname + '/public'))
+
 app.get("/", (req, res) => {
-  res.send("Pagina de Inicio");
-});
+  res.render("index")
+})
+
+app.get("/Formulario", (req, res) => {
+  res.render("Formulario")
+})
+
+app.get("/Prestamo_Registrado", (req, res) => {
+  res.render("Prestamo_Registrado")
+})
+
+//puerto que se va a ejecutar
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
